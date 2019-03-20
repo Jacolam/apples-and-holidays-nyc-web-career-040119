b0016a4c71b78c2f1883dfcd6d61e29e1e0fc077
Jacob  Lam <jacob.lam.work@gmail.com>
2019-03-20 14:52:50 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -66,7 +66,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do | season , holiday|
     puts season.to_s.capitalize + ":"
     holiday.each do |day , supplies|
-      " #{day.to_s.split(" ").collect(&:capitalize).join("")}: #{supplies.join(", ")} "
+     puts  " #{day.to_s.split(" ").collect(&:capitalize).join("")}: #{supplies.join(", ")} "
     end 
   end 
 
