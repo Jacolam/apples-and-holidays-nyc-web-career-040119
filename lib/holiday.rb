bd9f18c1ec84782bd5d82d1264cfae0428941a83
Jacob  Lam <jacob.lam.work@gmail.com>
2019-03-20 14:45:20 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -64,8 +64,8 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do | season , holiday|
-    season_string = ""
-    season_string = season.to_s.capitalize
+    puts season.to_s.capitalize
+    holiday.each do |
 
 end
 
